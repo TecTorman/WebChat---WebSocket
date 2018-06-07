aa912bbffdc3ec2ed273f85953e4737ea73ea289
TormanTec <tormantec@outlook.com>
2018-06-07 10:49:51 -0300

    Atualiza

---
+++ SocketChat/ChatManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocketChat
+{
+    public class ChatManager
+    {
+        public List<dynamic> Messages { get; set; } = new List<dynamic>();
+    }
+}
